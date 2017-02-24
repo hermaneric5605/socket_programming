18ce0fe3fc4ddaeea228f28d20cd1cd5f93f854e
hermaneric5605 <herman7279@gmail.com>
2017-02-24 09:11:12 -0500

    working

--- simple_server.rb
+++ simple_server.rb
@@ -1,9 +1,64 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket'
+require 'json'
 
-server = TCPServer.open(2000)  # Socket to listen on port 2000
-loop {                         # Servers run forever
-  client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
-  client.puts "Closing the connection. Bye!"
-  client.close                 # Disconnect from the client
-}
+
+server = TCPServer.open(2000) #socket problems on my machine
+
+loop do #server runs forever
+  client = server.accept
+
+#loops until whole header is grabbed
+  header = ""
+  while line = client.gets
+    header << line
+    break if header =~ /\r\n\r\n$/
+  end
+
+  #parses out method and path from status line
+  method = header.split[0]
+  path = header.split[1][1..-1]
+
+
+
+#if it's a GET request for a valid file, create a GET response
+  if method == "GET" && File.exist?(path)
+    response_body = (File.open(path, 'r')).read
+    response_head = "HTTP/1.0 200 OK\nContent-Length: #{response_body.length}\r\n\r\n"
+
+#if it's a valid POST request
+  elsif method == "POST" && File.exist?(path)
+
+  #iterate likewise to grab the request body
+    body = ""
+    while line = client.gets
+      body << line
+      break if body =~ /\r\n\r\n$/
+    end
+    body = body.chomp.chomp #remove the double newline
+
+
+  #parse out the JSON received into a block of HTML
+    params = {}
+    params = JSON.parse(body)
+    list_entries = ""
+    params["viking"].each do |key, value|
+      list_entries += "<li>#{key}: #{value}</li>\n      "
+    end
+
+
+  #open the HTML file and create a POST response by subbing in the parameters
+    file = File.open(path)
+    response_body = file.read.gsub("<%= yield %>", list_entries)
+    response_head = "HTTP/1.0 200 OK\nContent-Length: #{response_body.length}\r\n\r\n"
+
+  else
+    response_head = ("HTTP/1.0 404 Not Found\r\n\r\n")
+    response_body = ("Sorry, file not found.")
+  end
+
+#whatever the response is, output it here and close
+  client.puts(response_head)
+  client.puts(response_body)
+  client.close
+end
+Contact GitHub API Training Shop Blog About
--- tiny_web_browser.rb
+++ tiny_web_browser.rb
@@ -1,15 +1,63 @@
 require 'socket'
- 
-host = 'localhost'     # The web server
-port = 80                           # Default HTTP port
-path = "/index.html"                 # The file we want 
+require 'json'
 
-# This is the HTTP request we send to fetch a file
-request = "GET #{path} HTTP/1.0\r\n\r\n"
+host = 'localhost' #the web server
+port = 2000 # weird port issues on my machine
 
-socket = TCPSocket.open(host,port)  # Connect to server
-socket.print(request)               # Send request
-response = socket.read             # Read complete response
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2) 
-print body                         # And display it
+
+#choose a request
+puts "Welcome to your tiny web browser!"
+puts "Do you want to try a GET or a POST request?"
+print "> "
+choice = gets.chomp.upcase
+until choice == "GET" || choice == "POST"
+  puts "Your choices are GET or POST."
+  print "> "
+  choice = gets.chomp.upcase
+end
+
+
+case choice
+
+#send either a proper GET request for index html
+when "GET" then
+  request = "GET /index.html HTTP/1.0\r\n\r\n"
+
+#or get additional data for a POST request, then send it
+when "POST"
+  #input section
+  puts "Welcome to the Viking Raid Registration Form!"
+  print "Please enter your Viking's name: "
+  name = gets.chomp
+  print "Please enter your Viking's e-mail address: "
+  address = gets.chomp
+
+  #packs up data into a JSON for the request
+  viking_hash = {viking: {name: name, address: address}}
+  viking_json = viking_hash.to_json
+
+  #sets up post request with all necessary headers and line breaks
+request = <<POST_REQUEST
+POST /thanks.html HTTP/1.0
+From: #{address}
+Content-Type: application/json
+Content-Length: #{viking_json.length}
+\r\n\r\n#{viking_json}\r\n\r\n
+POST_REQUEST
+end
+
+
+
+
+#connects, requests and gets the whole response
+socket = TCPSocket.open(host, port)
+socket.print(request)
+response = socket.read
+
+#split response at first blank line into headers and body
+headers, body = response.split("\r\n\r\n", 2)
+
+#if response works, prints body, otherwise shows the errors
+puts headers =~ (/200 OK/) ? body : headers
+
+socket.close
